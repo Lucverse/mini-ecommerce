b6e04ff2137f9f554992dca248e24c147466eda7
Luc <Luc1fer.tms.ig@gmail.com>
2023-03-09 20:28:57 +0530

    updated cartpage using localstorage

--- src/Components/Cart/CartPage.jsx
+++ src/Components/Cart/CartPage.jsx
@@ -1,27 +1,70 @@
-// CartPage.js
-
-import { useSelector } from "react-redux";
-import { selectCartItems, selectCartTotal, selectCartItemCount } from "./cartSelectors";
-
+import React, { useState, useEffect } from "react";
 
 const CartPage = () => {
-  const cartItems = useSelector(selectCartItems);
-  const cartTotal = useSelector(selectCartTotal);
-  const cartItemCount = useSelector(selectCartItemCount);
+  const [cartItems, setCartItems] = useState([]);
 
+  useEffect(() => {
+    const items = JSON.parse(localStorage.getItem("cart")) || [];
+    setCartItems(items);
+  }, []);
+
+  const incrementQuantity = (item) => {
+    const newCartItems = [...cartItems];
+    const index = newCartItems.findIndex((x) => x.id === item.id);
+    newCartItems[index].quantity += 1;
+    setCartItems(newCartItems);
+    localStorage.setItem("cart", JSON.stringify(newCartItems));
+  };
+
+  const decrementQuantity = (item) => {
+    const newCartItems = [...cartItems];
+    const index = newCartItems.findIndex((x) => x.id === item.id);
+    if (newCartItems[index].quantity > 1) {
+      newCartItems[index].quantity -= 1;
+      setCartItems(newCartItems);
+      localStorage.setItem("cart", JSON.stringify(newCartItems));
+    }
+  };
+
+  const removeItem = (item) => {
+    const newCartItems = cartItems.filter((x) => x.id !== item.id);
+    setCartItems(newCartItems);
+    localStorage.setItem("cart", JSON.stringify(newCartItems));
+  };
+
+  const cartItemCount = cartItems.reduce(
+    (acc, item) => acc + item.quantity,
+    0
+  );
+
+  const cartTotal = cartItems.reduce(
+    (acc, item) => acc + item.price * item.quantity,
+    0
+  );
   return (
     <div>
       <h1>Cart</h1>
-      {cartItems.map((item) => (
-        <div key={item.id}>
-          <p>{item.name}</p>
-          <p>{item.price}</p>
-          <p>{item.quantity}</p>
-        </div>
-      ))}
-      <p>Total: ${cartTotal}</p>
+      <p>Total: ₹{cartTotal}</p>
       <p>Quantity: {cartItemCount}</p>
-    </div>
+      {cartItems.map((item) => (
+        <div className="ProductCard" style={{ color: 'black', backgroundColor: 'white' }}>
+          <div>
+            <img src={item.image} alt={item.title} />
+          </div>
+          <div className="ProductCard2">
+            <h2 >{item.title}</h2>
+            <h4 >₹{item.price}</h4>
+          </div>
+          <div>
+            <button onClick={() => decrementQuantity(item)}>-</button>
+            <span>{item.quantity}</span>
+            <button onClick={() => incrementQuantity(item)}>+</button>
+          </div>
+          <button onClick={() => removeItem(item)}>Remove</button>
+        </div>
+      ))
+      }
+    </div >
   );
 };
 
--- src/Components/Product/SingleProductPage.jsx
+++ src/Components/Product/SingleProductPage.jsx
@@ -1,36 +1,53 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
-function SingleProductPage() {
 
-    const { id } = useParams();
-    const [state, setState] = useState([]);
-    useEffect(() => {
-        fetch(`https://dbioz2ek0e.execute-api.ap-south-1.amazonaws.com/mockapi/get-products/${id}`)
-            .then((res) => res.json())
-            .then((data => {
-                setState(data.data);
-            }))
-    }, [id]);
-    return (
-        <div className="mainSingleProudctCard">
-            <div className="SingleProductCard">
-                <div className="product-image">
-                    <img  src={state.image} height="300px" alt={state.title} />
-                </div>
-                <div>
-                    <h1 >{state.title}</h1>
-                    <p >Price : ₹ {state.price}</p>
-                    <h3>{state.category}</h3>
-                    <p>Description</p>
-                    <p>Alskjdfm lxckj wsoi2 msc skldj20sdfasdlfjasd fasldfa sdfjlas lasjkdflasmd alskdcjasldkcmn29oijfa asbcdasdcAlskjdfm
-                        Alskjdfm lxckj wsoi2 msc skldj20sdfasdlfjasd fasldfa sdfjlas lasjkdflasmd alskdcjasldkcmn29oijfa asbcdasdcAlskjdfm
-                        Alskjdfm lxckj wsoi2 msc skldj20sdfasdlfjasd fasldfa sdfjlas lasjkdflasmd alskdcjasldkcmn29oijfa asbcdasdcAlskjdfm
-                    </p>
-                    <p >{state.brand}</p>
-                </div>
-            </div>
-            <button className="button-css">Add to Cart</button>
+function SingleProductPage() {
+  const { id } = useParams();
+  const [state, setState] = useState({});
+
+  const handleAddToCart = () => {
+    const cartItems = JSON.parse(localStorage.getItem("cart")) || [];
+    const existingItemIndex = cartItems.findIndex(item => item.id === state.id);
+    if (existingItemIndex !== -1) {
+      // Item already exists in the cart, update the quantity
+      cartItems[existingItemIndex].quantity += 1;
+    } else {
+      // Item doesn't exist in the cart, add it
+      const itemToAdd = {...state, quantity: 1};
+      cartItems.push(itemToAdd);
+    }
+    localStorage.setItem("cart", JSON.stringify(cartItems));
+  };
+
+  useEffect(() => {
+    fetch(`https://dbioz2ek0e.execute-api.ap-south-1.amazonaws.com/mockapi/get-products/${id}`)
+      .then((res) => res.json())
+      .then((data => {
+        setState(data.data);
+      }))
+  }, [id]);
+
+  return (
+    <div className="mainSingleProudctCard">
+      <div className="SingleProductCard">
+        <div className="product-image">
+          <img src={state.image} height="300px" alt={state.title} />
         </div>
-    )
+        <div>
+          <h1>{state.title}</h1>
+          <p>Price : ₹ {state.price}</p>
+          <h3>{state.category}</h3>
+          <p>Description</p>
+          <p>Alskjdfm lxckj wsoi2 msc skldj20sdfasdlfjasd fasldfa sdfjlas lasjkdflasmd alskdcjasldkcmn29oijfa asbcdasdcAlskjdfm
+            Alskjdfm lxckj wsoi2 msc skldj20sdfasdlfjasd fasldfa sdfjlas lasjkdflasmd alskdcjasldkcmn29oijfa asbcdasdcAlskjdfm
+            Alskjdfm lxckj wsoi2 msc skldj20sdfasdlfjasd fasldfa sdfjlas lasjkdflasmd alskdcjasldkcmn29oijfa asbcdasdcAlskjdfm
+          </p>
+          <p>{state.brand}</p>
+        </div>
+      </div>
+      <button className="button-css" onClick={handleAddToCart}>Add to Cart</button>
+    </div>
+  );
 }
+
 export default SingleProductPage;
