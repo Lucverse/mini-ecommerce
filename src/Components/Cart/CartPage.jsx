7bbf2a52e6bdfaee99bea68d98d29c4e0f896608
Luc <Luc1fer.tms.ig@gmail.com>
2023-03-09 21:15:43 +0530

    updated cart page

--- src/Components/Cart/CartPage.jsx
+++ src/Components/Cart/CartPage.jsx
@@ -23,8 +23,11 @@ const CartPage = () => {
       newCartItems[index].quantity -= 1;
       setCartItems(newCartItems);
       localStorage.setItem("cart", JSON.stringify(newCartItems));
+    } else {
+      removeItem(item);
     }
   };
+
 
   const removeItem = (item) => {
     const newCartItems = cartItems.filter((x) => x.id !== item.id);
@@ -41,28 +44,52 @@ const CartPage = () => {
     (acc, item) => acc + item.price * item.quantity,
     0
   );
+
+  const placeOrder = () => {
+    alert("Order placed successfully!");
+    setCartItems([]);
+    localStorage.removeItem("cart");
+  };
+
   return (
     <div>
       <h1>Cart</h1>
-      <p>Total: ₹{cartTotal}</p>
-      <p>Quantity: {cartItemCount}</p>
-      {cartItems.map((item) => (
-        <div className="ProductCard" style={{ color: 'black', backgroundColor: 'white' }}>
+      {
+        cartItemCount ?
           <div>
-            <img src={item.image} alt={item.title} />
+            <p>
+              Total: ₹{cartTotal} + ₹50 delivery charge
+            </p>
+            <p>Total Items: {cartItemCount}</p>
+            <div className="cartPage">
+              {cartItems.map((item) => (
+                <div
+                  className="SingleProductCard"
+                  style={{ color: "black", backgroundColor: "white" }}
+                  key={item.id}
+                >
+                  <div className="product-image">
+                    <img src={item.image} alt={item.title} />
+                  </div>
+                  <div >
+                    <h2>{item.title}</h2>
+                    <h4>₹{item.price}</h4>
+                    <span>
+                      <span style={{ fontWeight: "bold" }}>Quantity: </span>
+                      {item.quantity}
+                    </span>
+                    <div className="cart-buttons">
+                      <button onClick={() => decrementQuantity(item)}>-</button>
+                      <button onClick={() => incrementQuantity(item)}>+</button>
+                    </div>
+                  </div>
+                </div>
+              ))
+              }
+            </div>
+            <button onClick={placeOrder}>Place Order</button>
           </div>
-          <div className="ProductCard2">
-            <h2 >{item.title}</h2>
-            <h4 >₹{item.price}</h4>
-          </div>
-          <div>
-            <button onClick={() => decrementQuantity(item)}>-</button>
-            <span>{item.quantity}</span>
-            <button onClick={() => incrementQuantity(item)}>+</button>
-          </div>
-          <button onClick={() => removeItem(item)}>Remove</button>
-        </div>
-      ))
+          : <h1>No Items In Cart</h1>
       }
     </div >
   );
--- src/Components/Product/SingleProductPage.jsx
+++ src/Components/Product/SingleProductPage.jsx
@@ -2,52 +2,51 @@ import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 
 function SingleProductPage() {
-  const { id } = useParams();
-  const [state, setState] = useState({});
+    const { id } = useParams();
+    const [state, setState] = useState({});
 
-  const handleAddToCart = () => {
-    const cartItems = JSON.parse(localStorage.getItem("cart")) || [];
-    const existingItemIndex = cartItems.findIndex(item => item.id === state.id);
-    if (existingItemIndex !== -1) {
-      // Item already exists in the cart, update the quantity
-      cartItems[existingItemIndex].quantity += 1;
-    } else {
-      // Item doesn't exist in the cart, add it
-      const itemToAdd = {...state, quantity: 1};
-      cartItems.push(itemToAdd);
-    }
-    localStorage.setItem("cart", JSON.stringify(cartItems));
-  };
+    const handleAddToCart = () => {
+        const cartItems = JSON.parse(localStorage.getItem("cart")) || [];
+        const existingItemIndex = cartItems.findIndex((item) => item.id === state.id);
+        if (existingItemIndex !== -1) {
+            alert("This item is already in your cart!");
+        } else {
+            const itemToAdd = { ...state, quantity: 1 };
+            cartItems.push(itemToAdd);
+            localStorage.setItem("cart", JSON.stringify(cartItems));
+            alert("Item added to your cart!");
+        }
+    };
 
-  useEffect(() => {
-    fetch(`https://dbioz2ek0e.execute-api.ap-south-1.amazonaws.com/mockapi/get-products/${id}`)
-      .then((res) => res.json())
-      .then((data => {
-        setState(data.data);
-      }))
-  }, [id]);
 
-  return (
-    <div className="mainSingleProudctCard">
-      <div className="SingleProductCard">
-        <div className="product-image">
-          <img src={state.image} height="300px" alt={state.title} />
+    useEffect(() => {
+        fetch(`https://dbioz2ek0e.execute-api.ap-south-1.amazonaws.com/mockapi/get-products/${id}`)
+            .then((res) => res.json())
+            .then((data => {
+                setState(data.data);
+            }))
+    }, [id]);
+
+    return (
+        <div className="mainSingleProudctCard">
+            <div className="SingleProductCard">
+                <div className="product-image">
+                    <img src={state.image} height="300px" alt={state.title} />
+                </div>
+                <div>
+                    <h1>{state.title}</h1>
+                    <p><span style={{ fontWeight: 'bold' }}>Price: </span> ₹ {state.price}</p>
+                    <p><span style={{ fontWeight: 'bold' }}>Category: </span>{state.category}</p>
+                    <p><span style={{ fontWeight: 'bold' }}>Brand: </span>{state.brand}</p>
+                    <h3 style={{ textDecoration: 'underline' }}>Description</h3>
+                    <p style={{ fontFamily: 'italic', fontStyle: 'oblique' }}>
+                        Description
+                    </p>
+                </div>
+            </div>
+            <button className="button-css" onClick={handleAddToCart}>Add to Cart</button>
         </div>
-        <div>
-          <h1>{state.title}</h1>
-          <p>Price : ₹ {state.price}</p>
-          <h3>{state.category}</h3>
-          <p>Description</p>
-          <p>Alskjdfm lxckj wsoi2 msc skldj20sdfasdlfjasd fasldfa sdfjlas lasjkdflasmd alskdcjasldkcmn29oijfa asbcdasdcAlskjdfm
-            Alskjdfm lxckj wsoi2 msc skldj20sdfasdlfjasd fasldfa sdfjlas lasjkdflasmd alskdcjasldkcmn29oijfa asbcdasdcAlskjdfm
-            Alskjdfm lxckj wsoi2 msc skldj20sdfasdlfjasd fasldfa sdfjlas lasjkdflasmd alskdcjasldkcmn29oijfa asbcdasdcAlskjdfm
-          </p>
-          <p>{state.brand}</p>
-        </div>
-      </div>
-      <button className="button-css" onClick={handleAddToCart}>Add to Cart</button>
-    </div>
-  );
+    );
 }
 
 export default SingleProductPage;
