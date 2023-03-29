5a632cfa4bf87a6332201c8dccf77aef877a6f4c
Luc <Luc1fer.tms.ig@gmail.com>
2023-03-29 10:31:28 +0530

    update cart page,

--- src/Components/Cart/CartPage.jsx
+++ src/Components/Cart/CartPage.jsx
@@ -1,9 +1,16 @@
 import React, { useState, useEffect } from "react";
+import CartPageCard from "./CartPageCard";
+import ConfirmOverlay from "./ConfirmOverlay";
 import NoItem from "./NoItem";
+import OrderButton from "./OrderButton";
 
 const CartPage = () => {
   const [cartItems, setCartItems] = useState([]);
   const [orderState, setOrderState] = useState("default");
+  const [showConfirmOverlay, setShowConfirmOverlay] = useState(false);
+  const [confirmMessage, setConfirmMessage] = useState("");
+  const [onConfirm, setOnConfirm] = useState(() => { });
+  const [onCancel, setOnCancel] = useState(() => { });
 
   useEffect(() => {
     const items = JSON.parse(localStorage.getItem("cart")) || [];
@@ -21,8 +28,6 @@ const CartPage = () => {
       alert("You can buy only 9 items at once", "alert-warning");
     }
   };
-
-
   const decrementQuantity = (item) => {
     const newCartItems = [...cartItems];
     const index = newCartItems.findIndex((x) => x.id === item.id);
@@ -31,14 +36,20 @@ const CartPage = () => {
       setCartItems(newCartItems);
       localStorage.setItem("cart", JSON.stringify(newCartItems));
     } else {
-      if (window.confirm("This is the last item. Are you sure you want to remove it?")) {
+      const onCancel = () => {
+        setShowConfirmOverlay(false);
+      };
+      const onConfirm = () => {
         removeItem(item);
-      }
+        setShowConfirmOverlay(false);
+      };
+      const message = "This is the last item. Are you sure you want to remove it?";
+      setConfirmMessage(message);
+      setOnConfirm(() => onConfirm);
+      setOnCancel(() => onCancel);
+      setShowConfirmOverlay(true);
     }
   };
-
-
-
   const removeItem = (item) => {
     const newCartItems = cartItems.filter((x) => x.id !== item.id);
     setCartItems(newCartItems);
@@ -49,12 +60,10 @@ const CartPage = () => {
     (acc, item) => acc + item.quantity,
     0
   );
-
   const cartTotal = cartItems.reduce(
     (acc, item) => acc + item.price * item.quantity,
     0
   );
-
   const placeOrder = () => {
     const orderedItems = JSON.parse(localStorage.getItem("orderedItems")) || [];
     const mergedItems = [...cartItems, ...orderedItems];
@@ -64,10 +73,7 @@ const CartPage = () => {
     setOrderState("done");
     alert("Order Placed", "alert-success");
   };
-
-
-
-  function handleButtonClick() {
+  const handleButtonClick = () => {
     setOrderState("placing");
     setTimeout(() => {
       placeOrder();
@@ -77,7 +83,7 @@ const CartPage = () => {
     }, 4000);
   }
 
-  function alert(message, className) {
+  const alert = (message, className) => {
     const div = document.createElement("div");
     div.className = `alert ${className}`;
     div.appendChild(document.createTextNode(message));
@@ -87,58 +93,42 @@ const CartPage = () => {
     }, 1500);
   }
   return (
-    <div>
-      {
-        cartItemCount ?
-          <div>
-            <p>
-              Total: ₹{cartTotal} + ₹50 delivery charge
-            </p>
-            <p>Total Items: {cartItemCount}</p>
-            <div className="cartPage">
-              {cartItems.map((item) => (
-                <div
-                  className="SingleProductCard"
-                  style={{ color: "black", backgroundColor: "white" }}
-                  key={item.id}
-                >
-                  <div className="product-image">
-                    <img src={item.image} alt={item.title} />
-                  </div>
-                  <div >
-                    <h2>{item.title}</h2>
-                    <h4>₹{item.price}</h4>
-                    <span>
-                      <span style={{ fontWeight: "bold" }}>Quantity: </span>
-                      {item.quantity}
-                    </span>
-                    <div className="cart-buttons">
-                      <button onClick={() => decrementQuantity(item)}>-</button>
-                      <button onClick={() => incrementQuantity(item)}>+</button>
-                    </div>
-                  </div>
-                </div>
-              ))
-              }
-            </div>
-            <button
-              className={`place-order place-order--${orderState}`}
-              onClick={handleButtonClick}
-            >
-              <div className="default text">Place Order</div>
-              <div className="forklift">
-                <div className="upper"></div>
-                <div className="lower"></div>
-              </div>
-              <div className="box animation"></div>
-              <div className="done text">Done</div>
-            </button>
+    <>
+      {cartItemCount ? (
+        <div>
+          <p>
+            Total: ₹{cartTotal} + ₹50 delivery charge
+          </p>
+          <p>Total Items: {cartItemCount}</p>
+          <div className="cartPage">
+            {cartItems.map((item) => (
+              <CartPageCard
+                key={item.id}
+                item={item}
+                decrementQuantity={decrementQuantity}
+                incrementQuantity={incrementQuantity}
+              />
+            ))
+            }
           </div>
-          :
-          <NoItem />
-      }
-    </div >
+          <OrderButton
+            handleButtonClick={handleButtonClick}
+            orderState={orderState}
+          />
+        </div>
+      ) : (
+        <NoItem />
+      )}
+      {showConfirmOverlay && (
+        <ConfirmOverlay
+          message={confirmMessage}
+          onConfirm={onConfirm}
+          onCancel={onCancel}
+        />
+      )}
+    </>
   );
+
 };
 
 export default CartPage;
