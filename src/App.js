036e0ac937c076ceef7c78f9df18cfa166a0fae4
Luc <Luc1fer.tms.ig@gmail.com>
2023-04-08 10:52:00 +0530

    app.js updated | store reference added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
+import './App.css';
+import { useState } from 'react';
 import AllRoutes from "./Routes/AllRoutes";
 import Navbar from "./Components/Navbar/Navbar";
-export default function App() {
-  return <div className="App">
-    <Navbar />
-    <AllRoutes />
-  </div>;
+import { ctx } from './Components/Context/myContext';
+import LoginPage from "./Components/Login/LoginPage";
+
+function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  return (
+    <div className="App">
+      <ctx.Provider value={{ isLoggedIn, setIsLoggedIn }}>
+        {isLoggedIn && <Navbar />}
+        {
+          isLoggedIn ? <AllRoutes /> : <LoginPage />
+        }
+      </ctx.Provider>
+    </div>
+  );
 }
+export default App;
