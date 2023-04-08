6724500de76564bcae1328600eb3e49d1ebd5672
Luc <Luc1fer.tms.ig@gmail.com>
2023-04-08 12:01:58 +0530

    updated overlay

--- src/Components/Cart/ConfirmOverlay.jsx
+++ src/Components/Cart/ConfirmOverlay.jsx
@@ -6,11 +6,11 @@ const ConfirmOverlay = ({ message, onConfirm, onCancel }) => {
       <div className="confirm-box">
         <div className="message">{message}</div>
         <div className="button-group">
-          <button className="confirm-button" onClick={onConfirm}>
-            Confirm
-          </button>
           <button className="cancel-button" onClick={onCancel}>
             Cancel
+          </button>
+          <button className="confirm-button" onClick={onConfirm}>
+            Confirm
           </button>
         </div>
       </div>
