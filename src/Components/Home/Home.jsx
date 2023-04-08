7ac17973cd08fbced68555603583d8c7da0abe57
Luc <Luc1fer.tms.ig@gmail.com>
2023-04-08 10:53:14 +0530

    home page added

---
+++ src/Components/Home/Home.jsx
@@ -0,0 +1,9 @@
+import bg from '../Media/bg.jpg'
+function Home() {
+    return (
+        <div className="homeDiv" style={{backgroundImage:{bg}}}>
+            <h1>Home</h1>
+        </div>
+    )
+}
+export default Home;
