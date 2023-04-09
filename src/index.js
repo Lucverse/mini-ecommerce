2a4f7895f77888de141d171497e3e2be35c40eb7
Luc <Luc1fer.tms.ig@gmail.com>
2023-04-09 19:09:14 +0530

    removed unwanted redux store

--- src/index.js
+++ src/index.js
@@ -3,16 +3,15 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { Provider } from "react-redux";
-import store from "./Components/Cart/store";
 import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <Provider store={store}>
+  // <Provider store={store}>
     <React.StrictMode>
       <BrowserRouter>
         <App />
       </BrowserRouter>
     </React.StrictMode>
-  </Provider>
+  // </Provider>
 );
